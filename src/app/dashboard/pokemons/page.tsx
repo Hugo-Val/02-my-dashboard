5d358e0e310b5e2ec6e2bf711a6624f4c831aa01
Hugo Valencia <hvb1507@hotmail.com>
2024-03-02 18:34:42 -0600

    lesson 50

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,16 @@
-export default function PokemonsPage() {
+const getPokemons = async ( limit=20,offset=0 ) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+  const data = await response.json();
+  return data;
+};
+
+export default async function PokemonsPage() {
+
+    const pokemons = await getPokemons();
+
   return (
     <div>
-      <h1>Welcome to Pokemons Page</h1>
+      { JSON.stringify(pokemons) }
     </div>
   );
 }
