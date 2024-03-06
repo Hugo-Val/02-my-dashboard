6f44b61b12924d42be14ab2cbefe04eede9848da
Hugo Valencia <hvb1507@hotmail.com>
2024-03-06 15:49:08 -0600

    lesson 58

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,33 @@
+import { Pokemon } from "@/pokemons";
 
 interface Props {
     params : { id: string };
 }
 
-export default function PokemonPage({ params }: Props) {
+const getPokemon = async ( id : string ) : Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,{
+    cache : "force-cache" // TODO: Change this in future
+  });
+  const data = await pokemon.json();
+  console.log("Loading...", data.name);
+
+
+  return data;
+
+};
+
+
+
+export default async function PokemonPage({ params }: Props) {
+
+  const pokemon = await getPokemon(params.id);
+
   return (
     <div>
-      <h1>Welcome to Pokemon Page</h1>
+      <h1>Pokemon { params.id }</h1>
+      <div>
+        {JSON.stringify(pokemon)}
+      </div>
     </div>
   );
 }
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -3,3 +3,4 @@ export { PokemonGrid } from './components/PokemonGrids';
 
 export type { PokemonsResponse } from './interface/pokemons-response';
 export type { SimplePokemon } from './interface/simple-pokemon';
+export type { Pokemon } from './interface/pokemon';
