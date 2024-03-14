d9f522f9daf83d95f28d7d8f11bcf4c15e2a969a
Hugo Valencia <hvb1507@hotmail.com>
2024-03-14 11:04:39 -0600

    lesson 58

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,7 +1,19 @@
 import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface Props {
     params : { id: string };
+}
+
+export async function generateMetadata({ params } : Props ) : Promise<Metadata> {
+ 
+const { id, name } = await getPokemon(params.id);
+ 
+return {
+  title: `#${id} - ${name}`,
+  description: `This is the ${name} page`     
+}
+
 }
 
 const getPokemon = async ( id : string ) : Promise<Pokemon> => {
@@ -26,7 +38,7 @@ export default async function PokemonPage({ params }: Props) {
     <div>
       <h1>Pokemon { params.id }</h1>
       <div>
-        {JSON.stringify(pokemon)}
+        {pokemon.name}
       </div>
     </div>
   );
